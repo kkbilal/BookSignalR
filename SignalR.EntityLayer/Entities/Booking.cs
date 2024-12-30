55e32bd0d1e20846f68c7f2fdea2a82e2aa9acaa
kkbilal <115799048+kkbilal@users.noreply.github.com>
2024-12-30 19:07:23 +0300

    Add project files.

---
+++ SignalR.EntityLayer/Entities/Booking.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SignalR.EntityLayer.Entities
+{
+	public class Booking
+	{
+		public int BookID { get; set; }
+		public string Name { get; set; }
+		public string phone { get; set; }
+		public string Mail { get; set; }
+		public int PersonCount { get; set; }
+		public DateTime Date { get; set; }
+
+	}
+}
